db20d13b0c69fd35c2ebcda4c2bce4b64b8fc6df
Shahab Hesaraki <Shahabhes@gmail.com>
2022-12-09 12:59:31 -0500

    loginButton updated

--- client/src/components/Auth/login-button.js
+++ client/src/components/Auth/login-button.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const history = useHistory();
+  // const { loginWithRedirect } = useAuth0();
 
-  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+  return <Button onClick={() => history.push("/login")}>Log In</Button>;
 };
 
 const Button = styled.button`
